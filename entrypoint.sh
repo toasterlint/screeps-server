8c476b49d858bf29384490a4a4292a49d7117966
Henry Mohn <hmohniii@gmail.com>
2019-06-10 17:27:45 -0500

    Adding password option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,19 @@
 if [ -f "/screeps_data/package.json"]; then
     echo Starting Screeps
     cd /screeps_data
-    npx screeps start
+    if [ -z "$SCREEPS_PASS" ]; then
+        npx screeps start
+    else
+        npx screeps start --password $SCREEPS_PASS
+    fi
 else
     echo Initializing Screeps Data
     cd /screeps_data
     npx screeps init
     echo Starting Screeps
-    npx screeps start
+    if [ -z "$SCREEPS_PASS" ]; then
+        npx screeps start
+    else
+        npx screep start --password $SCREEPS_PASS
+    fi
 fi
